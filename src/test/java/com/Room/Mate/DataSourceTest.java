687e24ece36d180dbe3c997b3c3c9aaf221219d5
youber12345 <youber123@naver.com>
2025-01-05 23:42:32 +0900

    [DB]build.gradle dependencies 수정

--- src/test/java/com/Room/Mate/DataSourceTest.java
+++ src/test/java/com/Room/Mate/DataSourceTest.java
@@ -1,34 +1,31 @@
 package com.Room.Mate;
 
-import java.sql.Connection;
-
-import javax.sql.DataSource;
-
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
 
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@SpringBootTest
 public class DataSourceTest {
+
     @Autowired
     private DataSource dataSource;
+
     @Autowired
     private SqlSessionFactory sqlSessionFactory;
 
     @Test
     public void testConnection() {
-        try(Connection con = dataSource.getConnection();
-            SqlSession session =sqlSessionFactory.openSession();
-        ){
-            System.out.println("con =" + con);
-            System.out.println("session =" + session);
-        }catch (Exception e) {
+        try (Connection con = dataSource.getConnection();
+             SqlSession session = sqlSessionFactory.openSession()) {
+            System.out.println("Connection established: " + con);
+            System.out.println("SQL Session created: " + session);
+        } catch (Exception e) {
             e.printStackTrace();
-
-            // TODO: handle exception
         }
     }
 }
